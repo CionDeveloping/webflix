e4766e561d08810ff4de70b1ae67e632f6b3c97e
Johansen Data <46403752+CionDeveloping@users.noreply.github.com>
2022-02-19 22:46:16 +0100

    V5.8X - Backend  fiksing - radarr api
    radarr api integrering. Fikset endel bugs.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from tmdbv3api import Authentication
 import os
 
 
+
 tmdb = TMDb()
 tmdb.api_key = '672700e3d4dd3246c3c060a7ee138222'
 
@@ -21,10 +22,13 @@ app = Flask(__name__, static_url_path='/static')
 app.config['MAX_CONTENT_LENGTH'] = 900040 * 900000
 app.config['UPLOAD_EXTENSIONS'] = ['.mp4']
 app.config['UPLOAD_PATH'] = 'static/filmer'
+
+# Sørger for at filtype er rett - laget for /Leggtil delen
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-  
+
+# Hjem siden - lister populære filmer. 
 @app.route('/Hjem', methods=['GET'])
 def popular():
     page = request.args.get(get_page_parameter(), type=int, default=1)
@@ -32,6 +36,7 @@ def popular():
     pagination = Pagination(page=page, total=450)
     return render_template('hjem.html', Movies=popular_movies, pagination=pagination)
 
+#Film beskrivelse
 @app.route('/Movie_Detail', methods=['GET'])
 def filmdesc():
     movie_id = request.args.get('id')
@@ -45,31 +50,28 @@ def filmdesc():
         return render_template('filmen.html', Movies=m, Crews=crews,
                                youtube="", Similar=s)
 
+#Avspilleren for filmer
 @app.route('/Avspiller', methods=['GET'])
 def avspiller():
     movie_id = request.args.get('id')
     m = movie.details(movie_id)
     return render_template('filmviewer.html', Movies=m)
 
-
+#Søks funksjonen
 @app.route('/Search', methods=['GET'])
 def search():
     query = request.args.get('query')
     searching = movie.search(query)
     return render_template('search.html', Movies=searching)
 
-'''
-ALL KODE UNDER ER KUN TEST KODE OG IKKE I PRODUKSJON ENDA
-
-JOBBER MED EN DEL PROBLEMER FORTSATT.
-
-'''
+#film adder frontenden
 @app.route('/Leggtil', methods=['GET'])
 def filmader():
     movie_id = request.args.get('id')
     m = movie.details(movie_id)
     return render_template('filmadder.html', Movies=m)
 
+# Mulighet for å legge til filmer - lagres automatisk som /static/filmer/film_id.mp4 - film adder backend
 @app.route('/Leggtil', methods=['GET', 'POST'])
 def upload_files():
     movie_id = request.args.get('id')
@@ -93,12 +95,11 @@ def upload_files():
         filmfailed = ("Ingen fil? skjerp deg")
         return render_template('filmadder.html', filmlagttil=filmlagttil, Movies=m, filmfailed=filmfailed)
         
-
-
+#Loginn ikke i produksjon enda, men "fungerer"
 @app.route('/loginn', methods=['POST', 'GET'])
 def login_render():
     return render_template('loginn.html')
-
+#Loginn ikke i produksjon enda, men "fungerer"
 @app.route('/minside', methods=['POST', 'GET'])
 def login_render_post():   
     brukernavn = request.form["brukernavn"]
@@ -112,7 +113,18 @@ def login_render_post():
     details = account.details()
     return render_template('minside.html', auth=auth, details=details, sesskey=sesskey, apikey=apikey)
 
+#Radarr er i produksjon - ikke ferdigstilt, må etterhvert opprettes en webconfig for denne.
+@app.route('/Radarr', methods=['GET', 'POST'])
+def radarr():
+    from arrapi import RadarrAPI
+    baseurl = 'http://192.168.50.36:7979'
+    apikey = '8a975ad8f2b3492099ddfab557d66990'
+    radarr = RadarrAPI(baseurl, apikey)
+    movie_id = request.args.get('id')
+    movie = radarr.get_movie(tmdb_id=movie_id)
+    movie.add("/filmer", "Any")
+    return " ", 204
+
 if __name__ == '__main__':
     app.run(debug=True)
-
 
